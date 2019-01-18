f0f2f2023302c0c8d551da87df3cf51a801850a7
AngeredOne <the.nex.official@gmail.com>
2019-01-19 01:16:17 +0800

    Add files via upload

---
+++ TI-PreCompiler/build/CMakeFiles/TI-PreCompiler.exec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TI-PreCompiler.exec.dir/src/main.cpp.o"
+  "TI-PreCompiler.exec.pdb"
+  "TI-PreCompiler.exec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TI-PreCompiler.exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TI-PreCompiler/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nex/Документы/Work/TI-Engine/TI-PreCompiler
+# Build directory: /home/nex/Документы/Work/TI-Engine/TI-PreCompiler/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
